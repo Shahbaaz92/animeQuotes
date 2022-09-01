ad5f9024d5f832df16e1a9323451d3f3ddbff34f
Shahbaaz Athhar <59239856+Shahbaaz92@users.noreply.github.com>
2022-09-01 16:15:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+//Get Quotes from API
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const newQuote = document.getElementById("new-quote");
+const twitter = document.getElementById("twitter");
+const loader = document.getElementById("loader");
+const quoteContainer = document.getElementById("quote-container");
+
+let apiData = [];
+
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function stopLoading() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+async function getQuotes() {
+  loading();
+  const apiUrl = "https://animechan.vercel.app/api/random";
+  try {
+    const response = await fetch(apiUrl);
+    apiData = await response.json();
+    quote.innerHTML = `${apiData.quote}`;
+    author.innerHTML = `${apiData.character} <br> (${apiData.anime})`;
+  } catch (error) {
+    //Catch Error Here
+  }
+  stopLoading();
+}
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote.innerHTML} - ${author.innerHTML}`;
+  window.open(twitterUrl, "_blank");
+}
+
+//Before loading
+loading();
+
+//On Load
+getQuotes();
+
+//On button click
+newQuote.addEventListener("click", getQuotes);
+twitter.addEventListener("click", tweetQuote);
